399b60540fdeacbc6bcecc394a29af0fac3d1bb3
Brord van Wierst <brord@brord.net>
2024-08-06 16:37:39 +0200

    using iota-sdk

--- identity_resolver/src/resolution/resolver.rs
+++ identity_resolver/src/resolution/resolver.rs
@@ -356,6 +356,92 @@ where
   }
 }
 
+#[cfg(feature = "demia")]
+mod demia_handler {
+  use crate::ErrorCause;
+
+  use super::Resolver;
+  use identity_document::document::CoreDocument;
+  use identity_demia_core::DemiaDID;
+  use identity_demia_core::IotaDocument;
+  use identity_demia_core::IotaIdentityClientExt;
+  use std::collections::HashMap;
+  use std::sync::Arc;
+  impl<DOC> Resolver<DOC>
+  where
+    DOC: From<IotaDocument> + AsRef<CoreDocument> + 'static,
+  {
+    /// Convenience method for attaching a new handler responsible for resolving IOTA DIDs.
+    ///
+    /// See also [`attach_handler`](Self::attach_handler).
+    pub fn attach_demia_handler<CLI>(&mut self, client: CLI)
+    where
+      CLI: IotaIdentityClientExt + Send + Sync + 'static,
+      {
+        let arc_client: Arc<CLI> = Arc::new(client);
+  
+        let handler = move |did: DemiaDID| {
+          let future_client = arc_client.clone();
+          async move { future_client.resolve_did(&did).await }
+        };
+  
+        self.attach_handler(DemiaDID::METHOD.to_owned(), handler);
+      }
+
+      /// Convenience method for attaching multiple handlers responsible for resolving IOTA DIDs
+    /// on multiple networks.
+    ///
+    ///
+    /// # Arguments
+    ///
+    /// * `clients` - A collection of tuples where each tuple contains the name of the network name and its
+    ///   corresponding client.
+    ///
+    /// # Examples
+    ///
+    /// ```ignore
+    /// // Assume `smr_client` and `iota_client` are instances IOTA clients `iota_sdk::client::Client`.
+    /// attach_multiple_demia_handlers(vec![("smr", smr_client), ("iota", iota_client)]);
+    /// ```
+    ///
+    /// # See Also
+    /// - [`attach_handler`](Self::attach_handler).
+    ///
+    /// # Note
+    ///
+    /// - Using `attach_demia_handler` or `attach_handler` for the DEMIA method would override all
+    /// previously added clients.
+    /// - This function does not validate the provided configuration. Ensure that the provided
+    /// network name corresponds with the client, possibly by using `client.network_name()`.
+    pub fn attach_multiple_demia_handlers<CLI, I>(&mut self, clients: I)
+    where
+      CLI: IotaIdentityClientExt + Send + Sync + 'static,
+      I: IntoIterator<Item = (&'static str, CLI)>,
+    {
+      let arc_clients = Arc::new(clients.into_iter().collect::<HashMap<&'static str, CLI>>());
+
+      let handler = move |did: DemiaDID| {
+        let future_client = arc_clients.clone();
+        async move {
+          let did_network = did.network_str();
+          let client: &CLI =
+            future_client
+              .get(did_network)
+              .ok_or(crate::Error::new(ErrorCause::UnsupportedNetwork(
+                did_network.to_string(),
+              )))?;
+          client
+            .resolve_did(&did)
+            .await
+            .map_err(|err| crate::Error::new(ErrorCause::HandlerError { source: Box::new(err) }))
+        }
+      };
+
+      self.attach_handler(DemiaDID::METHOD.to_owned(), handler);
+    }
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use identity_iota_core::block::output::AliasId;
@@ -366,7 +452,6 @@ mod tests {
   use identity_iota_core::IotaDocument;
   use identity_iota_core::IotaIdentityClient;
   use identity_iota_core::IotaIdentityClientExt;
-
   use super::*;
 
   struct DummyClient(IotaDocument);
@@ -388,7 +473,7 @@ mod tests {
         Ok(self.0.clone())
       } else {
         Err(identity_iota_core::Error::DIDResolutionError(
-          iota_sdk::client::error::Error::NoOutput(did.to_string()),
+          iota_sdk::client::Error::NoOutput(did.to_string()),
         ))
       }
     }
